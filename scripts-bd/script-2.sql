9ef59f5c10626f36cd8b6d9faf7354693a0dbfbc
melendezgg <melendezgg@gmail.com>
2014-08-07 21:24:57 -0600

    Cambios a la BD terraba
    Se migran las tablas desde Voldemort de apartados postales e email por
    proveedores, mercancías, instituciones y observaciones por proveedor.  A
    observaciones por proveedor se le agregan fecha y id y el campo
    observación se pasa de text a varchar(max).  Se eliminan los comentarios
    sin texto (más de 2000).  El id se agrega para soportar varias
    observaciones por proveedor.

---
+++ scripts-bd/script-2.sql
@@ -0,0 +1,61 @@
+use comprared
+
+SELECT [NUMPROV]
+      ,[ID_REGISTRO]
+      ,[APDO] into terraba.dbo.apartados_postales_proveedores
+  FROM [COMPRARED].[dbo].[PROVEEDOR_APDOS]
+
+SELECT [NUMPROV]
+      ,[ID_REGISTRO]
+      ,[EMAIL]
+      ,[PORUSUARIO]
+      ,[OCULTO] into terraba.dbo.email_proveedores
+  FROM [COMPRARED].[dbo].[PROVEEDOR_EMAIL]
+
+SELECT [NUMPROV]
+      ,[CODMERC]
+      ,[CATALOGO]
+      ,[NORMA_CALIDAD] into terraba.dbo.mercancias_por_proveedor
+  FROM [COMPRARED].[dbo].[PROV_MERC]
+
+SELECT [ID_MINISTERIO]
+      ,[NUMPROV] into terraba.dbo.proveedores_por_institucion
+  FROM [COMPRARED].[dbo].[PROVEEDOR_MINISTERIO]
+
+SELECT [ID_MINISTERIO]
+      ,[DESCRIPCION]
+      ,[ID_REAL_MINISTERIO]
+      ,[COD_SICA]
+      ,[COD_CI]
+      ,[COD_CI_SIGAF]
+      ,[GOBIERNO_CENTRAL]
+      ,[cedula]
+      ,[ID_REGISTRO]
+      ,[MOSTRAR]
+      ,[SOCIEDAD] into terraba.dbo.instituciones
+  FROM [COMPRARED].[dbo].[MINISTERIO]
+
+SELECT [NUMPROV]
+      ,[OBSERVACION] into terraba.dbo.observaciones_por_proveedor
+  FROM [COMPRARED].[dbo].[PROVEEDOR_OBSERVACIONES]
+
+-- Agregar llave y fecha a las observaciones por proveedor y borrar observaciones sin texto
+
+use terraba
+
+alter table terraba.dbo.observaciones_por_proveedor
+alter column observacion varchar(max)
+
+delete terraba.dbo.observaciones_por_proveedor
+where OBSERVACIOn = ''
+
+alter table terraba.dbo.observaciones_por_proveedor
+add id int identity primary key not null
+
+alter table observaciones_por_proveedor
+add fecha datetime null
+
+select * from observaciones_por_proveedor
+
+--
+
