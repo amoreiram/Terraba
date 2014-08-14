f6b7f88d13ff1f0a9451f7274613253cfe88d023
gastonalfaro <gaston.alf@gmail.com>
2014-08-14 15:01:20 -0600

    Crear de tablas
    Crear tablas necesarias

---
+++ scripts-bd/script-6.sql
@@ -0,0 +1,66 @@
+USE COMPRARED
+GO
+select * into terraba..provincia from Provincia
+go
+select * into terraba..canton from Canton
+go
+select * into terraba..distrito from Distrito
+go
+select * into terraba..moneda from MONEDA
+go
+select * into terraba..emisor_financiero from EMISOR_FINANCIERO
+go
+select * into terraba..fuentes_de_financiamiento from FUENTE_FINANCIAMIENTO
+go
+select * into terraba..aerolineas from Aerolinea
+go
+select * into terraba..categorias_de_pymes from CATEGORIAS_PYMES
+go
+select * into terraba..codigos_postales from CodigoPostal
+go
+select * into terraba..estado_de_contratos from ESTADO_CONTRATO
+go
+select * into terraba..estado_de_ejecucion_garantias from ESTADO_EJECUCION_GARANTIA
+go
+select * into terraba..estado_de_garantias from ESTADO_GARANTIA
+go
+select * into terraba..estado_de_recursos from ESTADO_RECURSO
+go
+select * into terraba..estado_de_sesiones from ESTADO_SESION
+go
+select * into terraba..estado_solicitudes_publicacion from ESTADO_SOLICITUD_PUBLICACION
+go
+select * into terraba..estado_de_proveedores from ESTADOPROVEEDOR
+go
+select * into terraba..formas_de_pago from FORMA_PAGO
+go
+select * into terraba..gaceta from GACETA
+go
+select * into terraba..garantias_por_proveedor from GARANTIA_PROVEEDOR
+go
+select * into terraba..tipos_de_adendum from TIPO_ADENDUM
+go
+select * into terraba..tipos_de_cambio from TIPO_CAMBIO
+go
+select * into terraba..tipos_de_contratacion from TIPO_CONTRATACION
+go
+select * into terraba..tipos_de_documento from TIPO_DOCUMENTO
+go
+select * into terraba..tipos_de_garantia from TIPO_GARANTIA
+go
+select * into terraba..tipos_de_notificacion from TIPO_NOTIFICACION
+go
+select * into terraba..tipos_de_oferta from TIPO_OFERTA
+go
+select * into terraba..tipos_de_recurso from TIPO_RECURSO
+go
+select * into terraba..tipos_de_tarea from TIPO_TAREA
+go
+select * into terraba..tipos_de_termino from TIPO_TERMINO
+go
+select * into terraba..tipos_de_contrato from TIPOS_CONTRATO
+go
+select * into terraba..tipos_de_prorroga from TIPOS_PRORROGAS
+go
+select * into terraba..tipos_de_proveedor from TIPOS_PROVEEDOR
+go
